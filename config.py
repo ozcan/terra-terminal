2914c4c64f84f63259a73c589a8952dc4b20906d
Ozcan ESEN <ozcanesen@gmail.com>
2012-11-15 07:07:33 +0200

    ConfigManager class change

--- config.py
+++ config.py
@@ -24,35 +24,20 @@ import os
 class ConfigManager():
 
     def __init__(self):
-        self.config = ConfigParser.SafeConfigParser({'bar': 'Life', 'baz': 'hard'})
-        self.cfg_file = "main.cfg"
-        if not os.path.isfile(self.cfg_file):
-            self.config.read(self.cfg_file)
-            
-            self.config_data = {
-                'vertical-aligment': 0,
-                'horizontal-aligment': 0,
-                'height': 20,
-                'width': 100,
+        self.config = ConfigParser.SafeConfigParser({'bar': '10', 'baz': 'hard'})
+        self.cfg_file = 'main.cfg'
+        self.namespace = 'default'
+        self.config.read(self.cfg_file)
 
-            }
-
-    def read_conf(self, key, default):
-        print self.config.get('default', key)
-        
-    def get_conf(self, key):
-        if self.config_data.has_key(key):
-            return self.config_data[key]
-        print "Debug: %s key not found in config data." % (key)
-        return None
-
-    def set_conf(self, key, value):
-        if self.config_data.has_key(key):
-            self.config_data[key] = value
-            return True
-        print "Debug: %s key not found in config data." % (key)
-        return None
+    def get(self, key):
+        try:
+            return int(self.config.get(self.namespace, key))
+        except ValueError:
+            return self.config.get(self.namespace, key)
+        except ConfigParser.NoOptionError:
+            print "[DEBUG] No option '%s' found in file '%s'" % (key, self.namespace)
+            return None
        
 
 a = ConfigManager()
-print a.read_conf('width', 200)
+print a.get('bar')+10
